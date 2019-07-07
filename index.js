62a1dd97ad644c59380471061163062b27230918
Mykle jones <mykle.j91@gmail.com>
2019-07-07 20:00:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ function whisper(string) {
  console.log('hello')
  }
  
-  function sayHiToGrandma(string) {
-   return "I can't hear you!" 
-   console.log('hello')
-  }
+  function sayHiToGrandma(string){
+      if (string===string.toLowerCase()){
+        return "I can't hear you!"
+      }else if (string===string.toUpperCase()){
+       return "YES INDEED!" 
+      }else if (string==="I love you, Grandma."){
+      return "I love you"  
+      
     
  
    
